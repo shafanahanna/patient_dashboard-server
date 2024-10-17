c179f6880ec396b552a628ccca85264e2b1087c9
shafanahanna <150221290+shafanahanna@users.noreply.github.com>
2024-10-17 23:15:03 +0530

    updated

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ mongoose.connect(process.env.MONGODB_URI, )
 
 const app = express();
 app.use(express.json());
-app.use(cors()); 
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+); 
 
 app.use('/api/auth', authrouter);
 app.use('/api/', patientrouter);
