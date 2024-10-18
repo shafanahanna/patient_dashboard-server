207d07b15961141a677786c4d3a12dd129814653
shafanahanna <150221290+shafanahanna@users.noreply.github.com>
2024-10-18 11:50:48 +0530

    upadtion

--- routes/Patientroutes.js
+++ routes/Patientroutes.js
@@ -4,7 +4,7 @@ import { getPatients, addPatient, getPatientById, deletePatient } from "../contr
 import { verifyToken } from "../middleware/auth.js";
 
 const patientrouter = express.Router();
-// patientrouter.use(verifyToken)
+ patientrouter.use(verifyToken)
 patientrouter.post("/patients",  addPatient);
 patientrouter.get("/patients", getPatients);
 patientrouter.get("/patients/:id",getPatientById)
